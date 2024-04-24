12d5398d56edffe05de49d2ed170da54418b25c6
tokosugimoto <toko.sugimoto.l021@dirbato.co.jp>
2024-04-24 17:31:13 +0900

    チャプター4のコミット

---
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,21 @@
+package com.techacademy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+}
