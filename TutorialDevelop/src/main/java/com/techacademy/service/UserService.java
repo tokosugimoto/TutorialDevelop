f29224de10e9e6d6dd08506223e560043bac2410
tokosugimoto <toko.sugimoto.l021@dirbato.co.jp>
2024-04-25 09:21:11 +0900

    レッスン5-6コミット

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -17,5 +18,11 @@ public class UserService {
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+    
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
